5445e9097540cc6c1bf96bad3c9dca9071754fa7
Jonathan Kevin <2605720@sit.singaporetech.edu.sg>
2026-09-25 11:42:01 +0800

    fouth week second  post

--- INF1103-Labs/persistent_auditor.py
+++ INF1103-Labs/persistent_auditor.py
@@ -14,10 +14,43 @@ def load_orders():
         # Return an empty list if the file doesn't exist yet
         return []
 
-# --- Main Program (Phase A) ---
+def get_valid_quantity():
+    while True:
+        user_input = input("Enter Quantity: ")
+        try:
+            amount = int(user_input)
+            if amount < 0:
+                print("Please enter a positive number.")
+            else:
+                return amount
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
 orders = load_orders()
 
 print("Current Orders:")
 print()
 for order in orders:
     print(f"{order[0]}, {order[1]}, {order[2]}")
+
+print()
+
+# Get New Order Input
+product_name = input("Enter Product Name: ")
+quantity = get_valid_quantity()
+
+# Generate New Order ID
+if orders:
+    last_id = int(orders[-1][0])
+    new_id = str(last_id + 1)
+else:
+    new_id = "1001"
+
+# Add the new order to the list (Tracking History)
+new_order = [new_id, product_name, quantity]
+orders.append(new_order)
+
+# Display New Order Added
+print()
+print("New Order Added:")
+print(f"{new_id},{product_name},{quantity}")
